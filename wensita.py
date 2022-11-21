8821a79d0e582b74f40c2ca7ee4f7cd311f9bc52
wendytaNavarro <wendy.navarro@ucr.ac.cr>
2022-11-21 11:47:04 -0600

    wen

---
+++ wensita.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title("Hola Wensita")
+
+st.title("Hola Wensita")
+
+st.header("Primer encabezado")
+
+st.subheader("Primer encabezado")
+
+st.markdown("*Itálica*,**Negrita**, [UCR] https://www.ucr.ac.cr/")
+
+st.markdown("*Itálica*, **Negrita**, [UCR](https://www.ucr.ac.cr/)")
